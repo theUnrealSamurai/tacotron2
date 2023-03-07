ff6cf676365abaf69c81d68c21ba17bf8944f6f9
Roshan Saravanan <roshan.s@simpragma.com>
2023-03-07 10:59:53 +0530

    "modified the hparams to tf2"

--- hparams.py
+++ hparams.py
@@ -1,11 +1,15 @@
 import tensorflow as tf
 from text import symbols
 
+class HParams(object):
+    def __init__(self, **kwargs):
+        for k, v in kwargs.items():
+            self.__setattr__(k, v)
 
 def create_hparams(hparams_string=None, verbose=False):
     """Create model hyperparameters. Parse nondefault from given string."""
 
-    hparams = tf.contrib.training.HParams(
+    hparams = HParams(
         ################################
         # Experiment Parameters        #
         ################################
@@ -86,10 +90,10 @@ def create_hparams(hparams_string=None, verbose=False):
     )
 
     if hparams_string:
-        tf.logging.info('Parsing command line hparams: %s', hparams_string)
+        tf.compat.v1.logging.info('Parsing command line hparams: %s', hparams_string)
         hparams.parse(hparams_string)
 
     if verbose:
-        tf.logging.info('Final parsed hparams: %s', hparams.values())
+        tf.compat.v1.logging.info('Final parsed hparams: %s', hparams.__dict__)
 
     return hparams
